4dbb4707b4d78850d3a2d8fe5fddd6afa6859d6d
MohamadHarastani <mohmmad.harastani@gmail.com>
2023-09-15 19:38:17 +0200

    allows less templates and controling the template/distractor ratio

--- step4_CreateSnowballs.py
+++ step4_CreateSnowballs.py
@@ -1,4 +1,3 @@
-# remember to activate your eman2 conda environment before launching this script
 import os
 import glob
 import tqdm
@@ -6,16 +5,22 @@ import numpy
 import random
 
 # paramters to set
-number_of_snowballs = 100
+number_of_snowballs = 48
 dimentions = ['256', '256', '64']
 iterations = 5
-insersion_distances = [1, 2]
+insersion_distances = [0, 1]
 sigma = 2
 gray_level_threshold = 100
 threads = None
+
+# number of distractors for each template
+density_ratio = 3  # set it to 1 if your template is small, 2 or more if the tempalte is big
 #
 
 templates = list(glob.glob('volumes/templates/*.mrc'))
+# repleat the templates just in case a template is alone, or the number of templates are less than distractors
+templates = templates * 200
+
 distractors_and_frequencies = numpy.loadtxt('Frequencies.csv', delimiter=',', dtype='str')
 distractors_and_frequencies = distractors_and_frequencies.transpose()
 os.mkdir('snowballs/')
@@ -33,13 +38,17 @@ for snowball_number in tqdm.tqdm(range(number_of_snowballs)):
     random.shuffle(templates)
     # fix the names to include the path and _centered.mrc, and add the templates
     for i in range(len(distractors_and_frequencies)):
-        new_line = ['volumes/distractors/{}_centered.mrc'.format(distractors_and_frequencies[i, 0]), distractors_and_frequencies[i, 1]]
+        new_line = ['volumes/distractors/{}_centered.mrc'.format(distractors_and_frequencies[i, 0]),
+                    distractors_and_frequencies[i, 1]]
         molecules_list.append(new_line)
-        new_line = [templates[i], distractors_and_frequencies[i, 1]]
+        if i % density_ratio == 0:
+            new_line = [templates[i], distractors_and_frequencies[i, 1]]
+
         molecules_list.append(new_line)
+
     # now run the snowball
-    molecules = list(numpy.array(molecules_list)[:,0])
-    frequencies = list(numpy.array(molecules_list)[:,1])
+    molecules = list(numpy.array(molecules_list)[:, 0])
+    frequencies = list(numpy.array(molecules_list)[:, 1])
     arg = "python functions/multisnowball_python.py --mol '{}' --dim '{}' --frequencies '{}' --iterations {}" \
           " --coordinate_table {} --angle_table {} --output_volume {} --insersion_distances '{}' --sigma {}" \
           " --gray_level_threshold {} --threads {}".format(molecules, dimentions, frequencies, iterations,
