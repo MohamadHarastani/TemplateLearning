01be64ff1f0b0752a26510f99d793da91a908cb9
MohamadHarastani <mohmmad.harastani@gmail.com>
2023-09-25 00:23:04 +0200

    modified multisnowball for denser levels

--- functions/multisnowball_python.py
+++ functions/multisnowball_python.py
@@ -27,7 +27,7 @@
 
 import mrcfile
 from scipy.spatial.transform import Rotation
-from scipy.ndimage import binary_dilation, binary_closing
+from scipy.ndimage import binary_dilation, binary_closing, binary_erosion
 import scipy
 from skimage.morphology import ball
 import numpy as np
@@ -201,9 +201,19 @@ def snowball(mols, dim, frequencies, Iterations, coordinate_table, angle_table, 
                 rotated_binary = gaussian3d(rotated_molecule, sigma)
                 rotated_binary = (rotated_binary > gray_level_threshold).astype(np.int8)
                 # create the band-like shape around the molecule
-                out_layer = binary_dilation(rotated_binary, ball(insersion_distances[1]))
+                if insersion_distances[1] == 0:
+                    out_layer = binary_closing(rotated_binary, ball(1))
+                elif insersion_distances[1] < 0:
+                    out_layer = binary_closing(rotated_binary, ball(1))
+                    out_layer = binary_erosion(out_layer, ball(-insersion_distances[1]))
+                else:
+                    out_layer = binary_dilation(rotated_binary, ball(insersion_distances[1]))
+
                 if insersion_distances[0] == 0:
-                    in_layer = binary_closing(rotated_binary, ball(insersion_distances[1]))
+                    in_layer = binary_closing(rotated_binary, ball(1))
+                elif insersion_distances[0] < 0:
+                    in_layer = binary_closing(rotated_binary, ball(1))
+                    in_layer = binary_erosion(in_layer, ball(-insersion_distances[0]))
                 else:
                     in_layer = binary_dilation(rotated_binary, ball(insersion_distances[0]))
                 # -100 works as a penalty inside the band that should not touch any white space
